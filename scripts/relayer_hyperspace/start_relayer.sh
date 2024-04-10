a58f9915488bc01c06c57f9044bdec0a73a747cc
kienn6034 <kien@notional.ventures>
2024-04-10 18:45:27 +0700

    feat: add scripts running hyperspace

---
+++ scripts/relayer_hyperspace/start_relayer.sh
@@ -0,0 +1,1 @@
+RUST_LOG=hyperspace_cosmos=trace,hyperspace=trace,info /Users/kien6034/notional/projects/composable-ibc/target/release/hyperspace create-clients --config-a scripts/relayer_hyperspace/config-chain-a.toml --config-b scripts/relayer_hyperspace/config-chain-b.toml --config-core scripts/relayer_hyperspace/config-core.toml --delay-period 10
