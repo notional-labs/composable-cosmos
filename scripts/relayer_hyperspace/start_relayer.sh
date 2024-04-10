f06f493f3b41c9f719ef8173e17185bad0e01d68
kienn6034 <kien@notional.ventures>
2024-04-10 22:46:02 +0700

    feat: add scripts to start and clean up relayer

--- scripts/relayer_hyperspace/start_relayer.sh
+++ scripts/relayer_hyperspace/start_relayer.sh
@@ -1,1 +1,7 @@
-RUST_LOG=hyperspace_cosmos=trace,hyperspace=trace,info /Users/kien6034/notional/projects/composable-ibc/target/release/hyperspace create-clients --config-a scripts/relayer_hyperspace/config-chain-a.toml --config-b scripts/relayer_hyperspace/config-chain-b.toml --config-core scripts/relayer_hyperspace/config-core.toml --delay-period 10
+./scripts/relayer_hyperspace/run-composable.sh
+./scripts/relayer_hyperspace/run-picasso.sh
+
+# Waiting for nodes to start 
+sleep 20 
+
+RUST_LOG=hyperspace_cosmos=trace,hyperspace=trace,info /Users/kien6034/notional/projects/composable-ibc/target/release/hyperspace create-clients --config-a scripts/relayer_hyperspace/config-chain-a.toml --config-b scripts/relayer_hyperspace/config-chain-b.toml --config-core scripts/relayer_hyperspace/config-core.toml --delay-period 50
