2677f635498d6df44b85caaa9bfbe16d740a8c9f
kienn6034 <kien@notional.ventures>
2024-05-07 23:35:45 +0700

    chore: add hyperspace and cwgranpda wasm file

--- scripts/50/store-wasm-code.sh
+++ scripts/50/store-wasm-code.sh
@@ -4,7 +4,7 @@ DENOM="ppica"
 HOME_DIR="mytestnet"
 CHAINID="centauri-dev"
 BINARY=picad
-WASM_CONTRACT_PATH="/home/kien6034/notional/composable-ibc/target/wasm32-unknown-unknown/release/ics10_grandpa_cw.wasm"
+WASM_CONTRACT_PATH="$PWD/bin/ics10_grandpa_cw.wasm"
 
 # Wait for chain to start 
 echo "Waiting for chain to start..."
