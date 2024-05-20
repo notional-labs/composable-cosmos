727d9d9545de6aa2121325dee16487fcc15abaac
kienn6034 <kien@notional.ventures>
2024-05-20 21:00:53 +0700

    feat: scripts to migrate contract

--- scripts/50/migrate-wasm-code.sh
+++ scripts/50/migrate-wasm-code.sh
@@ -10,11 +10,11 @@ WASM_CONTRACT_PATH="bin/ics10_grandpa_cw.wasm"
 WASM_CLIENT_ID="08-wasm-0"
 
 HEX_CHECKSUM="3e743bf804a60e5fd1dfab6c61bba0f2e76cda260edc66d6b7b10691fb5096c1"
-$BINARY tx ibc-wasm migrate-contract $WASM_CLIENT_ID $HEX_CHECKSUM '{}' --title "store new wasm code"  --summary "none"  --from $KEY --keyring-backend test --chain-id $CHAINID  --deposit 10000000000${DENOM}  --home $HOME_DIR --gas 20002152622 --fees 20020166${DENOM} -y
+$BINARY tx ibc-wasm migrate-contract $WASM_CLIENT_ID $HEX_CHECKSUM {} --title "store new wasm code"  --summary "none"  --from $KEY --keyring-backend test --chain-id $CHAINID  --deposit 10000000000${DENOM}  --home $HOME_DIR --gas 20002152622 --fees 20020166${DENOM} -y
 
 sleep 6
 # $BINARY query gov proposals -o json > /tmp/proposals.json
-PROPOSAL_ID=5
+PROPOSAL_ID=6
 echo "Proposal ID is: $PROPOSAL_ID"
 
 # Validator vote yes
